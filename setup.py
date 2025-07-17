f3524308b9098b22376c266dde006e381f72d135
Antoine A. Ruzette <antoine.ruzette@gmail.com>
2025-07-17 13:01:02 -0400

    wip

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     package_data={
-        '': ['data/**/*'],
+        'evaluatesegmask': ['data/**/*'],
     },
     install_requires=[
         "numpy",
